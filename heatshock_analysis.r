697427a4f1bc365db6804d1af65aed5a6f2102cc
Peter Humburg <humburg@well.ox.ac.uk>
2015-01-21 15:35:14 +0000

    Added code to run analysis and generate report files.
    Reports are generated in html and pdf formats. Report files are copied
    to the root directory of the web server.

---
+++ heatshock_analysis.r
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+library(knitr)
+
+file.copy("html/waiting.html", "/usr/share/nginx/html/index.html", overwrite=TRUE)
+
+file.remove("heatshock_analysis.md", "heatshock_analysis.html", "heatshock_analysis.pdf")
+tryCatch(
+		knit("heatshock_analysis.Rmd"),
+		error=function(e){
+			cat("# We have a problem!", "R encountered the following issue while trying",
+					"to analise the heat shock data:", e, sep="\n", 
+					file=heatshock_analysis.md)
+		}
+)
+
+tryCatch(
+		pandoc("heatshock_analysis.md", config="default.pandoc"),
+		error=function(e){
+			cat("<!DOCTYPE html>", "<html>", "<head>", '<meta charset="UTF-8">',
+					"<title>Pandoc error</title>","</head>", "<body>", 
+					"<h1>We have a problem!</h1>", 
+					"<p>Pandoc encountered a problem while trying to generate the analysis report.</p>",
+					"<p>", e, "</p>",
+					"</body>","</html>", sep="\n", file="heatshock_analysis.html")
+		},
+		finally={
+			file.copy("heatshock_analysis.html", "/usr/share/nginx/html/index.html",
+					overwrite=TRUE)
+			if(file.exists("heatshock_analysis.pdf")){
+				file.copy("heatshock_analysis.pdf", 
+						"/usr/share/nginx/html/heatshock_analysis.pdf",
+						overwrite=TRUE)
+			}
+		}
+)
