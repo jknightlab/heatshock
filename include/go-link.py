9c16c26d582f43cf255dbcde37233ddbee787182
Peter Humburg <humburg@well.ox.ac.uk>
2015-07-29 11:53:34 +0100

    use *Str()* rather than bare string.

--- include/go-link.py
+++ include/go-link.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from pandocfilters import toJSONFilter, RawInline
+from pandocfilters import toJSONFilter, RawInline, Str
 from re import match
 
 """
@@ -14,7 +14,7 @@ def html(s):
 
 def linkGO(key, value, format, meta):
     if key == 'Str' and (format == 'html5' or format == 'html') and match("GO:\d{7}$", value) is not None:
-        return [html('<a href="http://amigo.geneontology.org/amigo/term/' + value + '">')] + value + [html('</a>')]
+        return [html('<a href="http://amigo.geneontology.org/amigo/term/' + value + '">')] + [Str(value)] + [html('</a>')]
 
 if __name__ == "__main__":
     toJSONFilter(linkGO)
